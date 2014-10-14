bec4a648e7d27e2e20a6a0b163f9406c12df3d66
Aurimas K <4urimas@gmail.com>
2014-10-15 00:10:23 +0200

    Comments

--- convert.rb
+++ convert.rb
@@ -1,8 +1,8 @@
 require 'json'
 
-langs = JSON.parse(File.read("languages_orig.json"))
-            .select  {|lang| lang.has_key?('iso_639_2b') && lang['type'] == "living"}
-            .collect {|lang| {lang['iso_639_2b'] => lang['name']} }
-            .inject(:merge)
+langs = JSON.parse(File.read("languages_orig.json"))                                    # Read and parse JSON file
+            .select  {|lang| lang.has_key?('iso_639_2b') && lang['type'] == "living"}   # Select only hashes that meet criteria
+            .collect {|lang| {lang['iso_639_2b'] => lang['name']} }                     # Collect needed values from hashes and make a new hash
+            .inject(:merge)                                                             # Change array of hashes into single hash
 
 File.open('languages.json', 'w') { |f| f.write(langs.to_json) }
